8513db406bbcc18bc6b683b103f120a7e427ce97
Varesa1 <necaticoskuncopur@gmail.com>
2022-06-13 16:22:40 +0300

    css fixed

--- src/App.js
+++ src/App.js
@@ -49,9 +49,9 @@ function App() {
           const url = `https://en.wikipedia.org/?curid=${result.pageid}`
           return (
             <div className="result" key={i}>
-             <h3>{result.title}</h3>
+              <h3>{result.title}</h3>
               <p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
-              <a href={url}  target="_blank" rel="noopener noreferrer">Read More</a>
+              <a href={url} target="_blank" rel="noopener noreferrer">Read More</a>
             </div>
           )
         })}
